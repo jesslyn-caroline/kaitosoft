dd8c450bb5c74d85596dada590317468057e5854
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-05-11 23:31:56 +0700

    fix: font-weight (from semibold to bold), color variable

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,9 +4,9 @@ function Navbar() {
     return (
         <div className={`w-full h-16 px-24 bg-white flex items-center justify-between fixed`}>
             <div className={`flex items-center space-x-2`}>
-                <h1 className={`font-semibold text-2xl`}>
-                    <span className={`text-primary`}>kaito</span>
-                    <span className={`text-[#2C74B3]`}>soft</span>
+                <h1 className={`font-bold text-2xl`}>
+                    <span className={`text-dark-blue`}>kaito</span>
+                    <span className={`text-bluish`}>soft</span>
                 </h1>
                 <img src="src/assets/up-chevron.png" alt="up-chevron"/>
             </div>
