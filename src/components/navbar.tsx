6cbb2be9409559b331ca4398df6d525dc0ec34bd
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-05-12 21:37:05 +0700

    fix: font weight

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,11 +1,13 @@
 
 
 function Navbar() {
+    localStorage.theme = "dark"
+
     return (
-        <div className={`w-full h-16 px-24 bg-white flex items-center justify-between fixed`}>
+        <div className={`w-full h-16 px-24 bg-white flex items-center justify-between fixed z-10`}>
             <div className={`flex items-center space-x-2`}>
-                <h1 className={`font-bold text-2xl`}>
-                    <span className={`text-dark-blue`}>kaito</span>
+                <h1 className={`font-semibold text-2xl`}>
+                    <span className={`text-dark-blue dark:text-white`}>kaito</span>
                     <span className={`text-bluish`}>soft</span>
                 </h1>
                 <img src="/images/up-chevron.png" alt="up-chevron"/>
