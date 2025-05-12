99d65705499eb495e2b279883c85f4f1d0b2eb4a
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-05-12 21:37:28 +0700

    fix: font weight add: motion on hero section

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -1,4 +1,4 @@
-
+import { motion } from "framer-motion"
 
 function Home() {
     return (
@@ -7,27 +7,35 @@ function Home() {
             {/* Hero Section */}
             <div className={`w-full h-screen px-24 pt-24 pb-10`}>
                 <div className={`w-full h-full bg-[#F4F8FB] rounded-[18px] flex flex-row`}>
-                    <div className={`w-1/2 px-18 flex flex-col justify-center space-y-4`}>
-                        <h1 className={`text-3xl font-bold text-black`}>
+                    <motion.div 
+                    initial={{opacity: 0, x: "-100%"}}
+                    whileInView={{opacity: 1, x: 0}}
+                    transition={{duration: 1}}
+                    className={`w-1/2 px-18 flex flex-col justify-center space-y-4`}>
+                        <h1 className={`text-3xl font-semibold text-black`}>
                             <span>Crafting </span>
                             <span className={`text-ball-blue`}>Digital Masterpiece</span>
                             <span>,</span>
                             <br/>
                             <span>One Line of Code at a Time</span>
                         </h1>
-                        <h2 className={`text-xl text-black`}>
+                        <h3 className={`text-xl text-black`}>
                             We turn your ideas into captivating modern websites, tailored to your need.
-                        </h2>
+                        </h3>
                         <button className={`w-fit px-4 py-2 bg-bluish rounded-[18px] flex flex-row items-center space-x-2`}>
                             <span className={`text-white text-lg font-semibold`}>Get Your Website</span>
                             <div className={`w-6 h-6 bg-white rounded-full`}>
                                 <i className={`ri-arrow-right-line`}></i>
                             </div>
                         </button>
-                    </div>
-                    <div className={`w-1/2 flex items-center justify-center`}>
+                    </motion.div>
+                    <motion.div
+                    initial={{opacity: 0, x: "100%"}}
+                    whileInView={{opacity: 1, x: 0}}
+                    transition={{duration: 1}}
+                    className={`w-1/2 flex items-center justify-center`}>
                         <img src="/images/hero-img.png" alt="hero-img"/>
-                    </div>
+                    </motion.div>
                 </div>
             </div>
 
