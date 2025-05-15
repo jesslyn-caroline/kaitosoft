0cfdc48e81924e26dfb1231a8600b260dc122e1d
Jesslyn Caroline <158556535+jesslyn-caroline@users.noreply.github.com>
2025-05-15 09:27:19 +0700

    remove: h-fit

--- src/screens/layout.tsx
+++ src/screens/layout.tsx
@@ -4,7 +4,7 @@ import Navbar from "../components/navbar.tsx"
 
 function Layout() {
     return (
-        <div className={`w-full h-fit bg-white flex justify-center`}>
+        <div className={`w-full bg-white flex justify-center`}>
             <div className={`max-w-[1624px] w-screen h-screen flex flex-col`}>
                 <Navbar/>
                 <Outlet/>
