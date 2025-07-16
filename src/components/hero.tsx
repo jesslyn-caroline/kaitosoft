6c2e869fe90704f1a6f11e9276a2211152c7d91d
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-07-16 21:11:41 +0700

    fix responsive

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,6 +1,6 @@
 function Hero() {
     return (
-        <section className={`w-full h-screen px-10 md:px-12 lg:px-24 pb-8 pt-18`}>
+        <section className={`w-full h-screen px-10 md:px-12 lg:px-24 pb-8 pt-18 flex flex-col justify-center`}>
             <div className={`w-full h-full md:bg-[#F4F8FB] rounded-xl flex flex-col md:flex-row`}>
                 <div className={`w-full md:w-5/10 md:px-10 lg:px-16 flex flex-col justify-center space-y-4`}>
                     <h1 className={`text-2xl md:text-xl lg:text-3xl font-bold`}>
@@ -10,7 +10,7 @@ function Hero() {
                         <br/>
                         Tailored for You
                     </h1>
-                    <h2 className={`text-sm md:text-md xl:text-lg`}>
+                    <h2 className={`text-sm xl:text-lg`}>
                         We build responsive, modern websites, from simple landing pages to scalable web solutions, designed to meet your needs.
                     </h2>
                     <button className={`text-md lg:text-lg text-white w-fit bg-accent-2 px-4 py-2 rounded-xl flex flex-row items-center space-x-2`}>
