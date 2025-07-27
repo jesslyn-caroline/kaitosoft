1e8322259256b8145a85398fe45c86fe03a9d502
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-07-27 22:54:27 +0700

    redo hero section

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,27 +1,26 @@
 function Hero() {
+    // alert(window.innerWidth)
     return (
-        <section className={`w-full h-screen px-10 md:px-12 lg:px-24 md:pb-8 pt-18`}>
-            <div className={`w-full h-full lg:bg-[#F4F8FB] rounded-xl flex flex-col md:flex-row items-center justify-center`}>
-                <div className={`w-full min-w-1/2 md:w-1/2 lg:px-16 flex flex-col justify-center space-y-4`}>
-                    <h1 className={`text-3xl md:text-xl lg:text-3xl font-bold`}>
-                        Crafting 
-                        <br/>
-                        <span className={`text-pretty text-secondary`}> Digital Masterpiece</span>, 
-                        <br/>
-                        Tailored for You
+        <section className={`w-full h-screen px-6 lg:px-16 pt-18 pb-12`}>
+            <div className={`w-full h-full lg:bg-[#F4F8FB] rounded-lg flex flex-col lg:flex-row`}>
+                <div className={`w-full lg:w-1/2 lg:px-14 flex flex-col justify-center items-start gap-2.5`}>
+                    <h1 className={`text-2xl lg:text-3xl font-bold`}>
+                        <span>Crafting</span> <br />
+                        <span className={`text-secondary`}>Digital Masterpiece</span><span>,</span> <br />
+                        <span>Tailored for You</span>
                     </h1>
-                    <h2 className={`text-md md:text-sm xl:text-lg`}>
+                    <p className={`text-sm lg:text-md`}>
                         We build responsive, modern websites, from simple landing pages to scalable web solutions, designed to meet your needs.
-                    </h2>
-                    <button className={`text-md md:text-sm lg:text-lg text-white w-fit bg-accent-2 px-4 md:px-3 lg:px-4 py-2 md:py-1 lg:py-2 rounded-xl flex flex-row items-center space-x-2`}>
-                        <span>Get Your Website</span>
-                        <div className={`w-6 h-6 bg-white rounded-full flex justify-center items-center`}>
-                            <i className={`ri-arrow-right-line text-black`}/>
+                    </p>
+                    <button className={`bg-accent-2 flex flex-row items-center px-4 py-1.5 gap-3 rounded-xl`}>
+                        <div className={`text-md text-white font-medium`}>Get Your Website</div>
+                        <div className={`w-6 h-6 bg-white rounded-full flex items-center justify-center`}>
+                            <i className={`ri-arrow-right-line`} />
                         </div>
                     </button>
                 </div>
-                <div className={`flex justify-center items-center p-6`}>
-                    <img src="/images/hero-img.png" alt="hero" className={`w-full md:w-md`}/>
+                <div className={`w-full mt-6 sm:mt-0 lg:w-1/2 lg:px-14 flex items-center justify-center`}>
+                    <img src="/images/hero.png" alt="hero" />
                 </div>
             </div>
         </section>
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,16 +2,13 @@
 
 function Navbar() {
     return (
-        <div className={`w-full h-16 px-10 md:px-12 lg:px-24 bg-white flex items-center justify-between fixed z-10`}>
+        <div className={`w-full h-16 px-6 lg:px-16 bg-white flex items-center justify-between fixed z-10`}>
             <div className={`flex items-center space-x-2`}>
                 <h1 className={`font-bold text-2xl`}>
                     <span className={`text-primary dark:text-white`}>kaito</span>
                     <span className={`text-accent-2`}>soft</span>
                 </h1>
                 <img src="/images/up-chevron.png" alt="up-chevron"/>
-            </div>
-            <div>
-                
             </div>
         </div>
     )
