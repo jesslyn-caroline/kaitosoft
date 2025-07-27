aad9bc1d9c36cdce5cd5d05cac43c42673f36341
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-07-27 22:58:39 +0700

    fix layout for mobile portrait

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -2,7 +2,7 @@ function Hero() {
     // alert(window.innerWidth)
     return (
         <section className={`w-full h-screen px-6 lg:px-16 pt-18 pb-12`}>
-            <div className={`w-full h-full lg:bg-[#F4F8FB] rounded-lg flex flex-col lg:flex-row`}>
+            <div className={`w-full h-full lg:bg-[#F4F8FB] rounded-lg flex flex-col lg:flex-row justify-center`}>
                 <div className={`w-full lg:w-1/2 lg:px-14 flex flex-col justify-center items-start gap-2.5`}>
                     <h1 className={`text-3xl lg:text-3xl font-bold`}>
                         <span>Crafting</span> <br />
@@ -19,7 +19,7 @@ function Hero() {
                         </div>
                     </button>
                 </div>
-                <div className={`w-full mt-6 sm:mt-0 lg:w-1/2 lg:px-14 flex items-center justify-center`}>
+                <div className={`w-full mt-8 sm:mt-0 lg:w-1/2 lg:px-14 flex items-center justify-center`}>
                     <img src="/images/hero.png" alt="hero" />
                 </div>
             </div>
