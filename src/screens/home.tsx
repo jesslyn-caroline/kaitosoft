e4f3df365ee5c94fac4e06c50f336e00e01d069b
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-07-28 22:56:38 +0700

    add Our Service section and scroll bar hidden

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -1,11 +1,13 @@
 // import { motion } from "framer-motion"
 // import { Link } from "react-router-dom"
 import Hero from "../components/hero"
+import OurServices from "../components/our-services"
 
 function Home() {
     return (
-        <div className={`w-full flex flex-col`}>
+        <div className={`w-full flex flex-col overflow-y-scroll scroll-bar-hidden`}>
             <Hero />
+            <OurServices />
         </div>
     )
 }
