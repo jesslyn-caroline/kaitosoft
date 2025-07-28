f9da0129e4f0fc294ec9591dc645776cf541161e
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-07-28 22:56:19 +0700

    new section

---
+++ src/components/our-services.tsx
@@ -0,0 +1,49 @@
+import { Link } from "react-router-dom";
+
+function OurServices() {
+    return (
+        <section className={`w-full h-screen md:h-fit lg:h-screen px-6 sm:px-10 lg:px-24 pb-12 flex flex-col gap-3`}>
+            <div>
+                <h2 className={`text-primary text-lg font-semibold`}>Our Services</h2>
+                <h1 className={`text-2xl font-bold`}>Websites Built for Your Goals</h1>
+            </div>    
+            <p className={`text-md`}>
+                We create websites that align with your needs, drive results, and help you grow. Our focus is on delivering digital
+                solutions that are both functional and impactful. Each website is built with attention to detail, ensuring it meets your
+                goals and adapts to future growth.
+            </p>
+            <p className={`text-md`}>
+                Solutions made to fit your needs. Choose what works best for you.
+            </p>
+            <div className={`grid grid-cols-2 gap-10`}>
+                <div className={`bg-accent-2 py-8 px-10 rounded-xl flex flex-col gap-1.5`}>
+                    <img src="/images/person.png" alt="person" className={`w-14`}/>
+                    <h3 className={`text-white text-lg font-semibold`}>Personal Website — Make Your Mark Online!</h3>
+                    <p className={`text-white text-md`}>
+                        A clean, modern site to showcase your skills and
+                        projects. Perfect for designers, developers,
+                        freelancers, or students.
+                    </p>
+                </div>
+                <div className={`bg-accent-2 py-8 px-10 rounded-xl flex flex-col gap-1.5`}>
+                    <img src="/images/success.png" alt="person" className={`w-14`}/>
+                    <h3 className={`text-white text-lg font-semibold`}>Business Page — Make Your Brand Stand Out!</h3>
+                    <p className={`text-white text-md`}>
+                        An elegant website built to highlight your
+                        services, engage visitors, and build trust with
+                        potential customers.
+                    </p>
+                </div>
+            </div>
+            <div className={`flex justify-end`}>
+                <Link to="/contact" className={`w-fit mt-8 flex items-center gap-2 border border-transparent border-b-black border-b-2`}>
+                    <span className={`text-lg font-semibold`}>Get Yours Now!</span>
+                    <i className={`ri-arrow-right-line text-xl`} />
+                </Link>
+            </div>
+            
+        </section>
+    );
+}
+
+export default OurServices
