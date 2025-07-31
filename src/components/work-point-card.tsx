fd123824804e133935940abcd722884e642dcfe1
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-07-31 23:52:16 +0700

    card component for how-we-works point

---
+++ src/components/work-point-card.tsx
@@ -0,0 +1,23 @@
+interface Props {
+    num: number,
+    point: string,
+    desc: string
+}
+
+function WorkPointCard({num, point, desc} : Props) {
+    return (
+        <div className={`bg-[#F4F8FB60] px-6 py-4 rounded-lg flex flex-col gap-2`}>
+            <div className={`flex flex-row items-center gap-3`}>
+                <div className={`w-8 h-8 bg-primary flex items-center justify-center rounded-lg text-white text-md font-bold`}>
+                    {num}
+                </div>
+                <span className={`text-primary text-lg font-bold`}>{point}</span>
+            </div>
+            <p className={`text-white text-md`}>
+                {desc}
+            </p>
+        </div>
+    ) 
+}
+
+export default WorkPointCard
