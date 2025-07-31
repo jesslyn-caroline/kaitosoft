e24242f309f234ee5abb5c87d4a2991be5025c35
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-07-31 20:54:58 +0700

    fix padding

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,10 +2,10 @@
 
 function Navbar() {
     return (
-        <div className={`w-full h-16 px-6 sm:px-10 lg:px-16 bg-white flex items-center justify-between fixed z-10`}>
+        <div className={`w-full h-16 px-6 sm:px-10 lg:px-24 bg-white flex items-center justify-between fixed z-10`}>
             <div className={`flex items-center space-x-2`}>
                 <h1 className={`font-bold text-2xl`}>
-                    <span className={`text-primary dark:text-white`}>kaito</span>
+                    <span className={`text-primary`}>kaito</span>
                     <span className={`text-accent-2`}>soft</span>
                 </h1>
                 <img src="/images/up-chevron.png" alt="up-chevron"/>
