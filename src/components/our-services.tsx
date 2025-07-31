e8779b3414c26b0690b255886eb81d9317fdeaa2
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-07-31 22:01:02 +0700

    fix padding for mobile landscape

--- src/components/our-services.tsx
+++ src/components/our-services.tsx
@@ -16,7 +16,7 @@ function OurServices() {
                 Solutions made to fit your needs. Choose what works best for you.
             </p> 
             <div className={`flex flex-col sm:grid sm:grid-cols-2 gap-4 sm:gap-6 lg:gap-10`}>
-                <div className={`bg-accent-2 py-6 md:py-8 px-6 sm:px-4 md:px-10 rounded-xl flex flex-col gap-1.5`}>
+                <div className={`bg-accent-2 py-6 md:py-8 px-6 sm:px-2 md:px-10 rounded-xl flex flex-col gap-1.5`}>
                     <img src="/images/person.png" alt="person" className={`w-14`}/>
                     <h3 className={`text-white text-lg font-semibold`}>Personal Website — Make Your Mark Online!</h3>
                     <p className={`text-white text-md`}>
@@ -25,7 +25,7 @@ function OurServices() {
                         freelancers, or students.
                     </p>
                 </div>
-                <div className={`bg-accent-2 py-6 md:py-8 px-6 sm:px-4 md:px-10 rounded-xl flex flex-col gap-1.5`}>
+                <div className={`bg-accent-2 py-6 md:py-8 px-6 sm:px-2 md:px-10 rounded-xl flex flex-col gap-1.5`}>
                     <img src="/images/success.png" alt="person" className={`w-14`}/>
                     <h3 className={`text-white text-lg font-semibold`}>Business Page — Make Your Brand Stand Out!</h3>
                     <p className={`text-white text-md`}>
