eddae008d1ea73d0427f8a0c389483909dae12f6
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-08-01 21:11:43 +0700

    fix card layout

--- src/components/work-point-card.tsx
+++ src/components/work-point-card.tsx
@@ -8,10 +8,10 @@ function WorkPointCard({num, point, desc} : Props) {
     return (
         <div className={`bg-[#F4F8FB60] px-6 py-4 rounded-lg flex flex-col gap-2`}>
             <div className={`flex flex-row items-center gap-3`}>
-                <div className={`w-8 h-8 bg-primary flex items-center justify-center rounded-lg text-white text-md font-bold`}>
+                <div className={`min-w-8 min-h-8 w-8 h-8 bg-primary flex items-center justify-center rounded-lg text-white text-md font-bold`}>
                     {num}
                 </div>
-                <span className={`text-primary text-lg font-bold`}>{point}</span>
+                <span className={`text-primary text-lg font-bold line leading-none`}>{point}</span>
             </div>
             <p className={`text-white text-md`}>
                 {desc}
